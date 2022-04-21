f00c2afaf4eb6ca7fee9467f3ebff9cc6b0f95b7
CalvinMoonly <calvin@moonlysoftware.com>
2022-04-21 12:52:25 +0200

    fix

--- src/main/java/com/dinnerinmotion/mockservice/MockServiceApplication.java
+++ src/main/java/com/dinnerinmotion/mockservice/MockServiceApplication.java
@@ -4,16 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.kafka.annotation.DltHandler;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.annotation.RetryableTopic;
-import org.springframework.kafka.support.KafkaHeaders;
-import org.springframework.messaging.handler.annotation.Header;
-import org.springframework.retry.annotation.Backoff;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.DltHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.annotation.RetryableTopic;
